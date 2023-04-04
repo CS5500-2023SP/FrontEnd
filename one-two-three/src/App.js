339e6f7d1ff073559e926eeb924624795c52042f
JianxunWang <wang.jianx@northeastern.edu>
2023-04-03 17:21:15 -0700

    Add router

--- one-two-three/src/App.js
+++ one-two-three/src/App.js
@@ -1,8 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Activity from './pages/Activity'
+
 function App() {
   return (
     <Routes>
-      <Route path="/" Component={Home} />
-      <Route path="/rules" Component={Rules} />
+      <Route path="/" element={<Home />} />
+      <Route path="/activity" element={<Activity />} />
     </Routes>
   )
 }
