34eb4dc71b09af28dff11aa60760100ec600a783
jiayanma <ma.jiaya@northeastern.edu>
2023-04-12 22:19:05 -0700

    page of most visited place

--- one-two-three/src/App.js
+++ one-two-three/src/App.js
@@ -1,14 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Activity from './pages/Activity'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Activity from './pages/Activity';
+import Place from './pages/Place';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/activity" element={<Activity />} />
+      <Route path="/place" element={<Place />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
